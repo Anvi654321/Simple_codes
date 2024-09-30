c24dd17dd80e540bf0ec5589c3ab9e4f043609b2
Anvita Shashi <anvi654321@gmail.com>
2024-09-29 20:33:07 -0400

    basic calculator

--- calculator.py
+++ calculator.py
@@ -1,2 +1,21 @@
-print("Hello")
-print("fukb.hugiyf")
+a=float(input("Enter first digit: "))
+op=input("Enter operator (+, -, *, /): ")
+b= float(input("Enter second digit: "))
+
+if op == "+":
+    c=a+b
+    print(c)
+elif op == "-":
+    d=a-b
+    print(d)
+elif op == "*":
+    e=a*b
+    print(e)
+elif op == "/":
+    if b == 0:
+        print ("oh no")
+    f=a/b
+    print(f)
+
+else:
+    print("invalid")
